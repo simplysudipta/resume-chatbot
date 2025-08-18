ee62de6b625be8aad56a277852f1d8ef5742068a
simplysudipta <simplysudipta@gmail.com>
2025-08-17 22:41:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ loader = PyPDFLoader(file_path)
 docs = loader.load_and_split()
 
 # --- Split text into chunks ---
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=500)
 chunks = text_splitter.split_documents(docs)
 
 # --- Create embeddings + retriever ---
