7158a9e52988c94eaf22f2a7cd5ff7705d596285
simplysudipta <simplysudipta@gmail.com>
2025-08-17 22:58:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ chunks = text_splitter.split_documents(docs)
 embeddings = OpenAIEmbeddings()
 vector_store = FAISS.from_documents(chunks, embeddings)
 #retriever = vector_store.as_retriever()
-retriever = vector_store.as_retriever(search_kwargs={"k": 5})
+retriever = vector_store.as_retriever(search_kwargs={"k": 8})
 
 # --- Custom Prompt ---
 custom_prompt = PromptTemplate(
